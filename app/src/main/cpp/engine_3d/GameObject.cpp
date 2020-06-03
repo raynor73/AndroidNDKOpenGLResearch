590a79cfdbfc065dbe0c3941b4b1004d6db23152
Igor Lapin <raynor73@gmail.com>
2020-06-03 19:44:54 +0100

    Sync

--- app/src/main/cpp/engine_3d/GameObject.cpp
+++ app/src/main/cpp/engine_3d/GameObject.cpp
@@ -4,11 +4,38 @@
 
 #include "GameObject.h"
 
-GameObject::GameObject(const std::string &name) {
-
-}
+GameObject::GameObject(const std::string &name) : m_name(name), m_isEnabled(true) {}
 
 GameObject::~GameObject() {
+    m_children.clear();
+    m_components.clear();
+}
 
+void GameObject::addChild(GameObject &child) {
+    auto childName = child.name();
+    if (m_children.count(childName) > 0) {
+        std::stringstream ss;
+        ss << "Trying to add " << childName << " game object second time";
+        throw std::domain_error(ss.str());
+    }
+    m_children[childName] = std::shared_ptr<GameObject>(&child);
+    child.m_parent = std::shared_ptr<GameObject>(this);
+}
+
+void GameObject::removeChild(GameObject &child) {
+    auto childName = child.name();
+    if (m_children.count(childName) == 0) {
+        std::stringstream ss;
+        ss << childName << " game object not found";
+        throw std::domain_error(ss.str());
+    }
+    m_children.erase(childName);
+    child.m_parent.reset();
+}
+
+int GameObject::s_cloneNamePostfix = 0;
+
+int GameObject::nextCloneNamePostfix() {
+    return s_cloneNamePostfix++;
 }
 
--- app/src/main/cpp/engine_3d/GameObject.h
+++ app/src/main/cpp/engine_3d/GameObject.h
@@ -17,41 +17,39 @@
 class GameObject {
 
 public:
-    GameObject(const std::string& name);
+    GameObject(const std::string &name);
     virtual ~GameObject();
 
-    const std::string& name() const { return m_name; }
+    const std::string &name() const { return m_name; }
 
     bool isEnabled() const { return m_isEnabled; }
     void setEnabled(bool isEnabled) { m_isEnabled = isEnabled; }
 
-    std::shared_ptr<GameObject> parent() { return m_parent.lock(); }
+    std::shared_ptr<GameObject> parent() { return m_parent; }
     const std::unordered_map<std::string, std::shared_ptr<GameObject>> children() { return m_children; }
 
-    void addChild(GameObject& child) {
-        /*if (_children.contains(child)) {
-            error("Trying to add ${child.name} game object second time")
-        }
-        _children += child
-        child._parent = this*/
-        if (m_children.count(child.name())) {
-            throw std::domain_error("Trying to add ${child.name} game object second time");
-        }
-    }
+    void addChild(GameObject &child);
+    void removeChild(GameObject &child);
+
+    void addComponent(GameObjectComponent &component);
+    void removeComponent(GameObjectComponent &component);
+
+    void update();
+
+    void clone();
+    void clone(const std::string &cloneName);
 
 private:
     bool m_isEnabled;
     std::string m_name;
 
-    std::weak_ptr<GameObject> m_parent;
+    std::shared_ptr<GameObject> m_parent;
     std::unordered_map<std::string, std::shared_ptr<GameObject>> m_children;
     std::unordered_map<std::string, std::shared_ptr<GameObjectComponent>> m_components;
 
-    std::string foo() {
-        std::stringstream ss;
-        ss << "123" << 42;
-        return ss.str();
-    }
+    static int s_cloneNamePostfix;
+
+    static int nextCloneNamePostfix();
 };
 
 
