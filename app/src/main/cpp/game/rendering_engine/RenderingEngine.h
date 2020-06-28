e3af398e632793afd1805e0dd758f49f56a3d4b6
Igor Lapin <raynor73@gmail.com>
2020-06-28 02:01:22 +0100

    Sync

---
+++ app/src/main/cpp/game/rendering_engine/RenderingEngine.h
@@ -0,0 +1,14 @@
+//
+// Created by Igor Lapin on 28/06/2020.
+//
+
+#ifndef GAME_RENDERINGENGINE_H
+#define GAME_RENDERINGENGINE_H
+
+
+class RenderingEngine {
+
+};
+
+
+#endif //GAME_RENDERINGENGINE_H
