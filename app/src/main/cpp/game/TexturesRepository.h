fa757c39efe7c86ee6d0facf51a2bf2f539d74c4
Igor Lapin <raynor73@gmail.com>
2020-07-24 09:02:28 +0100

    Implement text rendering, patch 9

---
+++ app/src/main/cpp/game/TexturesRepository.h
@@ -0,0 +1,12 @@
+//
+// Created by Igor Lapin on 23/07/2020.
+//
+
+#ifndef GAME_TEXTURES_REPOSITORY_H
+#define GAME_TEXTURES_REPOSITORY_H
+
+class TexturesRepository {
+
+};
+
+#endif //GAME_TEXTURES_REPOSITORY_H
