34d4292d1bcaaa72e7a4836d7e38568e31183e46
Igor Lapin <raynor73@gmail.com>
2020-12-10 09:37:55 +0000

    Add owned object tracking to ObjectPool

--- app/src/main/cpp/engine_3d/ObjectPool.h
+++ app/src/main/cpp/engine_3d/ObjectPool.h
@@ -16,13 +16,16 @@ class ObjectPool {
 
     std::function<std::shared_ptr<T>()> m_createObject;
     std::unordered_set<std::shared_ptr<T>> m_pool;
+    std::unordered_set<std::shared_ptr<T>> m_ownedObjects;
 
 public:
     ObjectPool(const std::function<std::shared_ptr<T>()>& createObject) : m_createObject(createObject) {}
 
     std::shared_ptr<T> obtain() {
         if (m_pool.size() == 0) {
-            return m_createObject();
+            auto newObject = m_createObject();
+            m_ownedObjects.insert(newObject);
+            return newObject;
         } else {
             auto object = *m_pool.begin();
             m_pool.erase(object);
@@ -33,6 +36,9 @@ public:
     void recycle(std::shared_ptr<T> object) {
         if (m_pool.count(object) > 0) {
             throw std::domain_error("Trying to recycle object that is already in the pool");
+        }
+        if (m_ownedObjects.count(object) == 0) {
+            throw std::domain_error("Trying to recycle object that is not owned by this pool");
         }
         m_pool.insert(object);
     }
